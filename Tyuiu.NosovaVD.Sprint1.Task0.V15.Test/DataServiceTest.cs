0c629ed3b9d2f034aa01995f0d8904fa04eadd2a
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-10-02 14:22:15 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.NosovaVD.Sprint1.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosovaVD.Sprint1.Task0.V15.Lib;
+namespace Tyuiu.NosovaVD.Sprint1.Task0.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(24,res);
+        }
+    }
+}
---
+++ Tyuiu.NosovaVD.Sprint1.Task0.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NosovaVD.Sprint1.Task0.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NosovaVD.Sprint1.Task0.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0107769-e0c9-4722-b4ec-a575d4b52a29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
